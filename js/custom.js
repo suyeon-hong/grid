0a88c949ed97f112e46b453f6f4afeb85ba7b6dd
bellahong95 <bellahong95@gmail.com>
2021-11-22 14:59:25 +0900

    마우스커서 효과 삭제, svg모션 추가

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,9 @@
 const item = $(".item");
-const img = item.find("img");
 
-
-img.on("mouseenter", function(){
-    let hasCursor = $(this).parent().find("img").hasClass("cursor");
-
-    if(hasCursor) return;
-    
-    $(this).closest(".article_inner").append(
-        $("<img src='img/cursor.png' class='cursor'>")
-    )
+item.on("mouseenter", function(){
+    $(this).find("svg").addClass("on");
 })
 
-img.on("mousemove", function(e){
-    let boxX = $(this).offset().left;
-    let boxY = $(this).offset().top;
-
-    let posX = (e.pageX - boxX) - 25;
-    let posY = (e.pageY - boxY) - 25;
-    
-    $("body").find(".cursor").css({
-        top: posY,
-        left: posX,
-    });
-});
-
 item.on("mouseleave", function(){
-    $(item).find(".cursor").remove();
+    $("svg").removeClass("on");
 });
